3c77dc344f07805e36f629e1a7350a812530f328
Kaan <kaanasutay10@gmail.com>
2024-01-28 02:16:57 +0300

    blog/user/page.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,5 +1,18 @@
+"use strict"
+
+import Link from "next/link";
+import Image from "next/image";
+
+
 const Nav = () => {
-  return <div>Nav</div>;
+  const isUserLoggedIn = true;
+  return 
+  <nav className="flex-between w-full mb-16 pt-3">
+    <Link href='/' className='flex gap-2 pt-3'>
+      <Image width={30} height={30} alt="logo" src="/assets/images/logo.svg" className="object-contain" />
+    </Link>
+
+  </nav>;
 };
 
 export default Nav;
