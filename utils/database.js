5b4108d97ff67c13aa7c542b3ac257b558eb418c
Kaan <kaanasutay10@gmail.com>
2024-01-28 20:23:11 +0300

    database

---
+++ utils/database.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(isConnected){
+        console.log("=> using existing database connection.")
+        return;
+    }
+    try{
+        await mongoose.connect(process.env.MONGODB_URL,{
+            dbName: 'promptHub',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }
+    catch(err){
+        console.log('error connecting to db', err)
+        throw err;
+    }
+}
